b84a33ec848117d5dda8ce2a86f5dec7ef8f36e2
jeppuu <jennamariakoskelo@gmail.com>
2021-06-10 14:05:02 +0300

    all pages done! (stats dont reduce)

--- javascript/master.js
+++ javascript/master.js
@@ -8,31 +8,27 @@ const dirtyness = document.getElementById('dirtyness-stat');
 const affection = document.getElementById('affection-stat');
 
 //the pet objects
-function Pet (name) {
-  this.name = name;
-}
-let dog = new Pet('Rufus');
-let cat = new Pet('Misty');
+function Pet (name) {           // -----------------------------------------------
+  this.name = name;             // | these objects don't really do anything yet, |
+}                               // | they're just here incase i want to use them |
+let dog = new Pet('Rufus');     // | for something else later!                   |
+let cat = new Pet('Misty');     // -----------------------------------------------
 let parrot = new Pet('Jacky');
 
-//pet's name, just hanging out here doing nothing
+//pet's name, just hanging out here doing nothing (again for later use maybe)
 petName = document.getElementById('pet-name-input').value;
 
-
-
-
-//functions for the action buttons
-//change the message and raise stats
+//functions for the action buttons: changes the message and raises stats
 function feed () {
   message.innerHTML = "Yum, my favourite treat!";
   hunger.value = hunger.value + 10;
 }
 function play () {
-  message.innerHTML = "*laugh* Playing together is fun!";
+  message.innerHTML = "Playing together is fun!";
   enjoyment.value = enjoyment.value + 10;
 }
 function bathe () {
-  message.innerHTML = "Thanks, I'm all clean now!";
+  message.innerHTML = "Thanks, now i'm clean!";
   dirtyness.value = dirtyness.value + 10;
 }
 function pat () {
